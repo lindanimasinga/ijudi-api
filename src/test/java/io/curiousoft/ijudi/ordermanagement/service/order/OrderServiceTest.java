510afc638f79e07e6d92f7ebcfcbf7407d5956f6
lindani-envel <79053807+lindani-envel@users.noreply.github.com>
2021-04-05 20:53:35 +0200

    fix timezone

--- src/test/java/io/curiousoft/ijudi/ordermanagement/service/order/OrderServiceTest.java
+++ src/test/java/io/curiousoft/ijudi/ordermanagement/service/order/OrderServiceTest.java
@@ -155,7 +155,7 @@ public class OrderServiceTest {
 
         LocalDateTime dateNow = LocalDateTime.now();
         MockedStatic<LocalDateTime> mocked = mockStatic(LocalDateTime.class);
-        mocked.when(LocalDateTime::now).thenReturn(dateNow.withHour(21).withMinute(45));
+        mocked.when(LocalDateTime::now).thenReturn(dateNow.withHour(6).withMinute(59));
 
         Set<Stock> stockItems = new HashSet<>();
         stockItems.add(new Stock("chips", 2, 10, 0, Collections.emptyList()));
