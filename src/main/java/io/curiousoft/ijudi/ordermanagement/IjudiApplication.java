093c708e12ee223bd4a4cc2e8a7c0297b369cd25
lindanimasinga <lindanimasinga@gmail.com>
2021-12-29 19:30:28 +0200

    aws sam

--- src/main/java/io/curiousoft/ijudi/ordermanagement/IjudiApplication.java
+++ src/main/java/io/curiousoft/ijudi/ordermanagement/IjudiApplication.java
@@ -2,13 +2,19 @@ package io.curiousoft.ijudi.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
 import java.util.TimeZone;
 
 @EnableSwagger2
@@ -16,7 +22,7 @@ import java.util.TimeZone;
 @EnableScheduling
 @EnableAsync
 @SpringBootApplication
-public class IjudiApplication {
+public class IjudiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IjudiApplication.class, args);
@@ -25,6 +31,21 @@ public class IjudiApplication {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+
+	/*
+	 * optimization - avoids creating default exception resolvers; not required as the serverless container handles
+	 * all exceptions
+	 *
+	 * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
+	 * an expensive ObjectMapper instance.
+	 *
+	 * To enable custom @ControllerAdvice classes remove this bean.
+	 */
+	@Bean
+	public HandlerExceptionResolver overrideHandlerExceptionResolver() {
+		return (HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) -> null;
 	}
 
 }
---
+++ src/main/java/io/curiousoft/ijudi/ordermanagement/StreamLambdaHandler.java
@@ -0,0 +1,39 @@
+package io.curiousoft.ijudi.ordermanagement;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+            LambdaContainerHandler.getContainerConfig().setInitializationTimeout(60_000);
+             try {
+//                 SpringBootLambdaContainerHandler.getAwsProxyHandler(DailySchedulerApp::class.java)
+                // For applications that take longer than 10 seconds to start, use the async builder:
+                 handler = new SpringBootProxyHandlerBuilder()
+                    .defaultProxy()
+                    .asyncInit(60_000)
+                    .springBootApplication(IjudiApplication.class)
+                    .buildAndInitialize();
+            } catch (ContainerInitializationException e) {
+                // if we fail here. We re-throw the exception to force another cold start
+                e.printStackTrace();
+                throw new RuntimeException("Could not initialize Spring Boot application", e);
+            }
+    }
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
