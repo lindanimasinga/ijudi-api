f063a8dbd3c58430bedeb8c1c79e53251d989064
lindanimasinga <lindanimasinga@gmail.com>
2022-04-17 11:55:17 +0200

    support yoco

---
+++ src/test/java/io/curiousoft/ijudi/ordermanagement/service/yoco/YocoPaymentProviderTest.java
@@ -0,0 +1,19 @@
+package io.curiousoft.ijudi.ordermanagement.service.yoco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class YocoPaymentProviderTest {
+
+    @Test
+    public void get_charge_id_from_description() {
+        String descrition = "yoco:1233435_token|charge:12345sdfsdf_charge";
+
+        //when
+        String chargeId = descrition.split("\\|")[1].replace("charge:", "");
+
+        //verify
+        assertEquals("12345sdfsdf_charge", chargeId);
+    }
+}
