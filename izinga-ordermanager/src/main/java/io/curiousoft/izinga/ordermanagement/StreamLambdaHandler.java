aae4d0482d25f1d918f03c4460fe90d6cf36e1b7
lindanimasinga <lindanimasinga@gmail.com>
2023-08-29 10:51:24 +0200

    fix later

--- izinga-ordermanager/src/main/java/io/curiousoft/izinga/ordermanagement/StreamLambdaHandler.java
+++ izinga-ordermanager/src/main/java/io/curiousoft/izinga/ordermanagement/StreamLambdaHandler.java
@@ -1,4 +1,4 @@
-package io.curiousoft.ijudi.ordermanagement;
+package io.curiousoft.izinga.ordermanagement;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
@@ -21,11 +21,11 @@ public class StreamLambdaHandler implements RequestStreamHandler {
              try {
 //                 SpringBootLambdaContainerHandler.getAwsProxyHandler(DailySchedulerApp::class.java)
                 // For applications that take longer than 10 seconds to start, use the async builder:
-                 handler = new SpringBootProxyHandlerBuilder()
-                    .defaultProxy()
-                    .asyncInit(60_000)
-                    .springBootApplication(IjudiApplication.class)
-                    .buildAndInitialize();
+                 handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+                        .springBootApplication(IjudiApplication.class)
+                        .defaultProxy()
+                        .asyncInit()
+                        .buildAndInitialize();
             } catch (ContainerInitializationException e) {
                 // if we fail here. We re-throw the exception to force another cold start
                 e.printStackTrace();
