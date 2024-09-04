d372edc0e980bda62dbabc443d912c84641d9184
lindanimasinga <lindanimasinga@gmail.com>
2024-09-04 17:13:02 +0200

    make expiry optional

--- izinga-qrcode-generator/src/main/kotlin/io/curiousoft/izinga/qrcodegenerator/promocodes/model/UserPromoDetails.kt
+++ izinga-qrcode-generator/src/main/kotlin/io/curiousoft/izinga/qrcodegenerator/promocodes/model/UserPromoDetails.kt
@@ -3,4 +3,4 @@ package io.curiousoft.izinga.qrcodegenerator.promocodes.model
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
-class UserPromoDetails(val userId: String, val promo: String, val verified: Boolean, val amount: BigDecimal, val expiry: LocalDateTime, val orderId: String)
+class UserPromoDetails(val userId: String, val promo: String, val verified: Boolean, val amount: BigDecimal, val expiry: LocalDateTime? = null, val orderId: String)
