319018be6ee6cf0cb7e3db187e7ef32e298e8c6a
lindanimasinga <lindanimasinga@gmail.com>
2025-01-23 02:23:03 +0200

    fix kotlin files

--- izinga-ordermanager/src/main/java/io/curiousoft/izinga/ordermanagement/stores/event/StoreDeletedEvent.kt
+++
@@ -1,9 +0,0 @@
-package io.curiousoft.izinga.ordermanagement.stores.event
-
-import io.curiousoft.izinga.commons.model.StoreProfile
-import org.springframework.context.ApplicationEvent
-
-abstract class StoreDeletedEvent(source: Any, val storeProfile: StoreProfile) :
-    ApplicationEvent(
-        source
-    )
