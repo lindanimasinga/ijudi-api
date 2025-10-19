3c5092aee52885ca53a9e2540373d88eb5c67fd0
lindanimasinga <Lindanimasinga@gmail.com>
2025-10-19 08:30:34 +0200

    Update YocoPaymentInitiate.kt

--- izinga-yoco-pay/src/main/kotlin/io/curiousoft/izinga/yocopay/api/YocoPaymentInitiate.kt
+++ izinga-yoco-pay/src/main/kotlin/io/curiousoft/izinga/yocopay/api/YocoPaymentInitiate.kt
@@ -4,4 +4,14 @@ import io.curiousoft.izinga.yocopay.PaymentMetadata
 import java.math.BigDecimal
 
 data class YocoPaymentInitiate(var amount: BigDecimal, val currency: String = "ZAR", val successUrl: String,
-                               val metadata: PaymentMetadata)
+                               val metadata: PaymentMetadata, val lineItems: List<YocoLineItem>? = null);
+
+data class YocoLineItem(
+    val displayName: String,
+    val quantity: Int,
+    val pricingDetails: YocoPricingDetails,
+    val description: String? = null)
+
+data class YocoPricingDetails(
+    val price: BigDecimal
+)
